cbd2618626f657774aa25ea4699891014bdf22c9
Yashasvi <ryashasvi77@gmail.com>
2025-09-27 00:48:58 +0530

    Update Problem 15.java

---
+++ Problem 15.java
@@ -0,0 +1,26 @@
+//Leap Year Validation  
+import java.util.Scanner;   
+class Problem15 {
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a Leap Year");
+        }
+        scanner.close();
+    }
+}
